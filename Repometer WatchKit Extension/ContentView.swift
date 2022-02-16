29e3aafd3ed86019d6649a46af120591b8ee04f2
Sam Perlmutter <sam.perlmutter227@gmail.com>
2022-02-15 20:05:34 -0500

    Added compose button and list view

--- Repometer WatchKit Extension/ContentView.swift
+++ Repometer WatchKit Extension/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Button("Buzz", action: {
+            WKInterfaceDevice.current().play(WKHapticType.click)
+        })
             .padding()
     }
 }
--- Repometer/ContentView.swift
+++ Repometer/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    struct Workout: Identifiable {
+        let name: String
+        let id = UUID()
+    }
+    
+    var workouts = [
+        Workout(name: "Plank"),
+        Workout(name: "Leg lift"),
+        Workout(name: "Bridge")
+    ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List (workouts) {
+                Text($0.name)
+            }
+            .navigationTitle("Workouts")
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Spacer()
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(systemName: "square.and.pencil")
+                    })
+                }
+            }
+        }
     }
 }
 
